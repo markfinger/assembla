b0224e9329fe38b7591efbe0d5f6bcaaf2896197
Mark Finger <markfinger@gmail.com>
2012-02-18 21:39:41 +1100

    Bumped version to 1.1.0.

--- setup.py
+++ setup.py
@@ -2,15 +2,12 @@ import os
 from setuptools import setup, find_packages
 
 # Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "assembla",
-    version = "1.0.2",
+    version = "1.1.0",
     packages = find_packages(),
 
     install_requires = [
