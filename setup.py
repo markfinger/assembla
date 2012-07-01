1b60d37c79fd5da505fe9312756e4eebc1464ddb
Mark Finger <markfinger@gmail.com>
2012-07-01 20:34:08 +1000

    Bumping the version to 1.2.3.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ Full documentation at http://github.com/markfinger/assembla
 
 setup(
     name = 'assembla',
-    version = '1.2.1',
+    version = '1.2.3',
     packages = find_packages(),
 
     install_requires = [
