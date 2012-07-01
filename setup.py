3fb889c3129f6a10ee22d27887cfb139b30cfbe2
Mark Finger <markfinger@gmail.com>
2012-07-01 18:46:17 +1000

    Moving PYPI_README.rst into setup.py. Cleaned up setup.py a bit and bumped the version to 1.2.1.

--- setup.py
+++ setup.py
@@ -1,45 +1,56 @@
 import os
 from setuptools import setup, find_packages
 
-# Utility function to read the README file.
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+PYPI_RESTRUCTURED_TEXT_INFO = \
+"""
+====================================================
+Assembla: Python wrapper for Assembla's API
+====================================================
+
+An easy to use wrapper around the Assembla API.
+
+Basic Example
+-------------
+
+::
+
+	from assembla import API
+
+	assembla = API(auth=('Username', 'Password',))
+
+	print assembla.space(name='Big Project').ticket(number=201).status_name
+
+
+More documentation and source at http://github.com/markfinger/assembla
+"""
 
 setup(
-    name = "assembla",
-    version = "1.2.0",
+    name = 'assembla',
+    version = '1.2.1',
     packages = find_packages(),
 
     install_requires = [
         'requests>=0.7.4',
         'lxml>=2.3.1',
     ],
-
-    package_data = {
-        'assembla': [
-        ]
-    },
-
-    entry_points = {
-    },
+    package_data = {'assembla': []},
+    entry_points = {},
 
     # metadata for upload to PyPI
-    author = "Mark Finger",
-    author_email = "markfinger@gmail.com",
-    description = (
-        "Python wrapper for the Assembla API"
-    ),
-    license = "BSD",
+    author = 'Mark Finger',
+    author_email = 'markfinger@gmail.com',
+    description = 'An easy to use wrapper around the Assembla API',
+    license = 'BSD',
     platforms=['any'],
-    keywords = "Assembla API",
-    url = "https://github.com/markfinger/assembla/",
-    long_description = read('PYPI_README.rst'),
+    keywords = 'Assembla API',
+    url = 'http://github.com/markfinger/assembla/',
+    long_description = PYPI_RESTRUCTURED_TEXT_INFO,
     classifiers = [
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "Topic :: Software Development",
-        "Operating System :: OS Independent",
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'Operating System :: OS Independent',
         'Programming Language :: Python',
-        "License :: OSI Approved :: BSD License",
+        'License :: OSI Approved :: BSD License',
     ],
 )
